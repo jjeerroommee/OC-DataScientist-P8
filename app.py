0f52f7f2308dc5b68d9fc7ae6b9757410c2297fa
jjeerroomme <jer.me>
2024-06-07 08:40:49 +0200

    ajout shap values dans retour predict

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import os
 import sys
 import pandas as pd
 from flask import Flask, render_template, jsonify, request
+import shap
 
 sys.path.append('./functions')
 from my_functions import load_model
+from my_functions import load_background
 
 app = Flask(__name__)
 
@@ -24,23 +26,37 @@ def classif():
         
     # Gets a dataframe of features from the post request's body
     json_input = request.get_json()
-    data = pd.DataFrame(json_input['data'], index=json_input['id']
-    
-    # Creates a response object
+   
+    data = pd.DataFrame(json_input['data'], index=[json_input['id']])
+    for i, col in enumerate(data.columns) :
+        if data.iloc[0, i] == '' :
+            data[col] = None
+            data[col] = pd.to_numeric(data[col])
+            
+    # Response part 1 : probability of default
     # [0] because it is launched on a single client
     # [1] because we want to extract the probability of the client to be a bad client
     proba_echec = model.predict_proba(data)[0][1] 
 
+    # Response part 2 : text label
     if proba_echec > 0.5 :
         classe = 'refusé'
     else :
         classe = 'accepté'
-    
+
+    # Response part 3 : shap values
+    shap_bg = load_background('./models')
+    explainer = shap.TreeExplainer(model, shap_bg, feature_names=shap_bg.columns, model_output='probability')
+    shap_values = explainer(data)
+
     # Returns the response object as json
     return jsonify({
         'classe': classe, 
-        'proba_echec': proba_echec
+        'proba_echec': proba_echec,
+        'shap_values' : shap_values[0].values.tolist(),
+        'shap_base_values' : shap_values[0].base_values
     })
+
 
 
 if __name__ == '__main__':
--- functions/my_functions.py
+++ functions/my_functions.py
@@ -1,5 +1,6 @@
 import joblib
 import os
+import pandas as pd
 
 def load_model(path_to_models):
     # 'model_selection.txt' contains the name of the joblib file we want to use
@@ -10,3 +11,8 @@ def load_model(path_to_models):
     model = joblib.load(os.path.join(path_to_models, model_dump))
     
     return model
+
+def load_background(path_to_models):
+    # load the model's training Data : we will use it as basckground data in shap values calculation
+    shap_bg = pd.read_csv(os.path.join(path_to_models, 'shap_background.csv'), sep=";")
+    return shap_bg
